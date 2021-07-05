7ed5fb4d5b98c24ce1928d458782e54a880f7a17
unknown <alecs2art@gmail.com>
2021-07-05 13:09:10 -0700

    created questions array

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,56 @@
 // TODO: Include packages needed for this application
 
+const Choices = require("inquirer/lib/objects/choices");
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    },
+    {
+        type: 'input',
+        name: 'projectName',
+        message: 'What is your projects name?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a short description of your project.'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license should your project have?'
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None']
+    },
+    {       
+        type: 'list',
+        name: 'dependencies',
+        message: 'What command should be run to install dependencies?'  
+    },
+    {
+        type: 'list',
+        name: 'tests',
+        message: 'What command should be run to run tests?'    
+    },
+    {
+        type: 'input',
+        name: 'repo',
+        message: 'What does the user need to know about using the repo?'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'What does the user need to know about contributing to the repo?'    
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
